72a65d99fce40ab734d0476818b1c99d8143ec9d
bmliupan <bmliupan@qq.com>
2021-09-11 17:17:03 +0800

    add project file resolution

--- 3_sourcecode/cpp/mainwindow.cpp
+++ 3_sourcecode/cpp/mainwindow.cpp
@@ -2,12 +2,15 @@
 #include "ui_mainwindow.h"
 #include <QTimer>
 #include <QFileDialog>
+#include <QTextStream>
+#include <QList>
+
 
 #define TIMER_TIMEOUT   (500)
 
 #define printNormal(x)              ui->textBrowser->setTextColor("blue");\
                                     ui->textBrowser->append(x);
-#define printWarnning(x)             ui->textBrowser->setTextColor("red");\
+#define printWarnning(x)            ui->textBrowser->setTextColor("red");\
                                     ui->textBrowser->append(x);
 
 
@@ -61,11 +64,36 @@ void MainWindow::on_loadButton_clicked()
 void MainWindow::on_buildButton_clicked()
 {
     QFile file(filePath);
+    uint8_t flag = 0;
+    uint8_t num = 0;
+    QList<QString> linkPathList;
+    QList<QString> dependPathList;
 
-    if(!file.open(QIODevice::ReadOnly)) {
+    if(!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
         printWarnning("读取工程文件失败！");
         return;
     }
     printNormal("工程文件解析中...");
+    QTextStream prjStream(&file);
+    while (!prjStream.atEnd()) {
+        QString str = prjStream.readLine();
+        if (str.contains("[LINK]", Qt::CaseSensitive)) {
+           flag = 1;
+        }
+        if (str.contains("[DEPEND]", Qt::CaseSensitive)) {
+           flag = 2;
+        }
+        if ((flag == 1) && str.contains("ITEM", Qt::CaseSensitive)) {
+           num = str.indexOf("=") + 1;
+           linkPathList.append(str.mid(num));
+        }
+        if ((flag == 2) && str.contains("ITEM", Qt::CaseSensitive)) {
+           num = str.indexOf("=") + 1;
+           dependPathList.append(str.mid(num));
+        }
+        printNormal(str);
+    }
+    file.close();
 
+    return;
 }
